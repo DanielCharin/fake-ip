222a0b994f8d0998c215e9ebc699c8609f29cc40
f1kus97 <tsarin97@gmail.com>
2020-02-09 13:39:48 +0200

    WebRTC initialization

---
+++ src/app/components/webrtc/webrtc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-webrtc',
+  templateUrl: './webrtc.component.html',
+  styleUrls: ['./webrtc.component.scss']
+})
+export class WebrtcComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
