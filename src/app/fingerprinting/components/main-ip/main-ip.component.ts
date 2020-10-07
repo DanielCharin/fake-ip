48294a86cef6105df6e90c123ff3aef3406f89cc
f1kus97 <tsarin97@gmail.com>
2020-10-07 16:09:50 +0300

    Finishing with TimeZone

--- src/app/fingerprinting/components/main-ip/main-ip.component.ts
+++ src/app/fingerprinting/components/main-ip/main-ip.component.ts
@@ -1,23 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {IpAddressService} from '../../services/ip-address.service';
 import { findFlagUrlByIso3Code } from 'country-flags-svg';
-
-class IPData {
-  ip: string;
-  country: string;
-  country_code?: string;
-  country_code3: string;
-  continent_code?: string;
-  city?: string;
-  region?: string;
-  latitude?: string;
-  longitude?: string;
-  accuracy?: number;
-  timezone: string;
-  organization?: string;
-  asn?: number;
-  organization_name?: string;
-}
+import {IPData} from '../../../model/IPData';
 
 @Component({
   selector: 'app-main-ip',
@@ -25,14 +9,14 @@ class IPData {
   styleUrls: ['./main-ip.component.scss']
 })
 export class MainIpComponent implements OnInit {
-  ipData: IPData = {ip: 'n/a', country: 'n/a', country_code3: 'n/a', timezone: 'n/a'};
+  ipData: IPData = {ip: 'n/a', country: 'n/a', country_code3: 'n/a'};
   countryFlagSrc: '';
 
   constructor(private ipAddressService: IpAddressService) {}
 
   ngOnInit(): void {
     this.ipAddressService.getIPData().subscribe(next => {
-      this.ipData = next;
+      this.ipData = new IPData(next);
       this.countryFlagSrc = findFlagUrlByIso3Code(this.ipData.country_code3);
     });
   }
--- src/app/fingerprinting/components/timezone/timezone.component.ts
+++ src/app/fingerprinting/components/timezone/timezone.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {IpAddressService} from '../../services/ip-address.service';
+import {IPData} from '../../../model/IPData';
+import * as moment from 'moment-timezone';
 
 @Component({
   selector: 'app-timezone',
@@ -9,9 +12,31 @@ export class TimezoneComponent implements OnInit {
   localTZ: string;
   systemTZ: string;
 
-  constructor() { }
+  constructor(private ipAddressService: IpAddressService) { }
 
   ngOnInit(): void {
-    this.systemTZ = new Date().toTimeString();
+    const systemTZ = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    this.ipAddressService.getIPData().subscribe(next => {
+      const localTZ = new IPData(next).timezone;
+      this.setTzValues(systemTZ, localTZ);
+    });
+  }
+
+  // TODO: Unit tets
+  setTzValues(system: string, local: string): void {
+    let systemOffsetInHours;
+    let localOffsetInHours;
+
+    systemOffsetInHours = moment().tz(system).utcOffset() / 60 > 0 ?
+      'UTC+' + moment().tz(system).utcOffset() / 60 :
+      'UTC' + moment().tz(system).utcOffset() / 60;
+
+    localOffsetInHours = moment().tz(local).utcOffset() / 60 > 0 ?
+      'UTC+' + moment().tz(local).utcOffset() / 60 :
+      'UTC' + moment().tz(local).utcOffset() / 60;
+
+    this.systemTZ = system + ' ' + systemOffsetInHours;
+    this.localTZ = local + ' ' + localOffsetInHours;
   }
 }
